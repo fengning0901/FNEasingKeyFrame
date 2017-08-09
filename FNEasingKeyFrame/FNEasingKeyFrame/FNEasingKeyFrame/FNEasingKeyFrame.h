aba9417960095a909612d67c796823d52849942c
冯宁 <nobita@xfenzi.com>
2017-08-09 22:05:26 +0800

    no message

--- FNEasingKeyFrame/FNEasingKeyFrame/FNEasingKeyFrame/FNEasingKeyFrame.h
+++ FNEasingKeyFrame/FNEasingKeyFrame/FNEasingKeyFrame/FNEasingKeyFrame.h
@@ -63,8 +63,9 @@ typedef NS_ENUM(NSInteger, FNEasingKeyFrameAnimationType) {
 };
 
 @interface FNEasingKeyFrame : NSObject
-
+// 根据总帧数和动画曲线类型直接生成动画帧序列
 + (NSArray<NSNumber*>*)keyFramesForAnimationType:(FNEasingKeyFrameAnimationType)type withNumberOfKeyFrames:(NSInteger)number;
+// 根据当前进度位置与动画曲线类型生成某一帧的位置
 + (double)singleKeyFrameForAnimationType:(FNEasingKeyFrameAnimationType)type withProgressRate:(double)rate;
 
 @end
